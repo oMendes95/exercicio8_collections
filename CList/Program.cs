c259edf0bb69c045fb9652b6484573bf5b3dfce8
pdrlcx <mendesv460@gmail.com>
2022-07-25 17:38:05 -0300

    Collections Adicionado

---
+++ CList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World Lista!");
+
+            List<string> animais = new List<string>();
+            
+            animais.Add("Urso");
+            animais.Add("Leão");
+            animais.Add("Tigre");
+            animais.Add("Pantera");
+
+            animais.Insert(1, "Hiena");
+
+            animais.Remove("Pantera");
+
+            animais.RemoveAt(1);
+
+            foreach (string animais1 in animais)
+            {
+
+                Console.WriteLine(animais1);
+
+            }
+
+        }
+    }
+}
---
+++ CQueue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            Queue<string> pessoas = new Queue<string>();
+            pessoas.Enqueue("Jhojan Julio");
+            pessoas.Enqueue("João Paulo");
+            pessoas.Enqueue("Marcos Leonardo");
+            pessoas.Enqueue("Angelo Gabriel");
+            pessoas.Enqueue("Rodrigo Fernandez");
+
+            pessoas.Peek("Jhojan Julio");
+
+            pessoas.Count;
+            
+
+
+
+
+
+
+            foreach
+
+
+
+        }
+    }
+}
